83bc8511453b15c7ace767b6cf66584bdf04f927
IOleg-sus <guchkooleg7@gmail.com>
2024-06-01 13:18:22 +0300

    Finished game

--- function.h
+++ function.h
@@ -4,9 +4,10 @@
 #include <string>
 
 // Function d:/gitnext/tic-tac-toe/function.h:symbol:(17,0)-(23,1)
-void symbol(char symbol , int n)
+void symbol(char symbol, int n)
 {
-    for(int i = 0 ; i < n ; i++){
+    for (int i = 0; i < n; i++)
+    {
         std::cout << symbol;
     }
     std::cout << std::endl;
@@ -23,7 +24,43 @@ void drawfield(char board[3][3])
     std::cout << " " << board[2][0] << "  |  " << board[2][1] << "   |  " << board[2][2] << std::endl;
     std::cout << "    |      |     " << std::endl;
 }
+// # TODO : need to fix
+int checkwin(char board[3][3])
+{
 
+    if (board[0][0] == board[0][1] && board[0][1] == board[0][2])
+    {
+        return 1;
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    {
+        return 1;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    {
+        return 1;
+    }
+    if (board[1][0] == board[1][1] && board[1][1] == board[1][2])
+    {
+        return 1;
+    }
+    if (board[2][0] == board[2][1] && board[2][1] == board[2][2])
+    {
+        return 1;
+    }
+    if (board[0][0] == board[1][0] && board[1][0] == board[2][0])
+    {
+        return 1;
+    }
+    if (board[0][1] == board[1][1] && board[1][1] == board[2][1])
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 void game(char board[3][3], std::string player1, std::string player2, int position, int position2, bool stop)
 {
     while (stop != true)
@@ -120,6 +157,18 @@ void game(char board[3][3], std::string player1, std::string player2, int positi
                 std::cout << "Please enter a number between 1 and 9" << std::endl;
                 break;
             }
+        }
+        checkwin(board);
+        if (checkwin(board) == 1)
+        {
+            stop = true;
+            std::cout << "Player " << player1 << " wins!" << std::endl;
+        }
+        else
+        {
+
+            stop = false;
+            std::cout << "Game drawn" << std::endl;
         }
     }
 }
